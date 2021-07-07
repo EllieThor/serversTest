aacf5ecc32399bd4283d01f7655c44d10dfea53e
EllieThor <elliethor18@gmail.com>
2021-07-07 22:54:26 +0300

    add functualety to online checkbox

--- client/src/app/services/servers-service.service.ts
+++ client/src/app/services/servers-service.service.ts
@@ -9,6 +9,7 @@ export class ServersServiceService {
   _servers: Array<Server> = [];
   result: any = {};
   _searchInput: string = '';
+  _onlyOnline: boolean = false;
   constructor(public apiService: ApiService) {}
 
   async getAllServers(url: string) {
@@ -22,11 +23,13 @@ export class ServersServiceService {
     this.result = (await this.apiService.createPostService(url, ob)) as any;
     console.log('result: ', this.result);
     console.log('url: ', url, '  ob: ', ob);
-    if (this.result.affectedRows === 1) {
+    if (this.result.affectedRows === 1)
       this.getAllServers('/servers/getAllServers');
-    }
-    // } else {
-    //   null;
-    // }
+  }
+
+  async onlyOnline() {
+    if (this._onlyOnline == true) this._onlyOnline = false;
+    else this._onlyOnline = true;
+    console.log('this._onlyOnline: ', this._onlyOnline);
   }
 }
